dd5fc8a29650f6b2fb07a9231075613030cb8447
Souvik-droid <80137576+Souvik-droid@users.noreply.github.com>
2023-10-26 12:57:56 +0530

    learnt about react element and react component

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-   const parent = React.createElement(
-    'div',
-    {id: 'parent'},[
-    React.createElement('div',{id:'child'}, 
-        [
-            React.createElement('h1', {}, 'text for h1'),
-            React.createElement('h2', {}, 'text for child 2')
-        ]),
+const parent = React.createElement(
+'div',
+{id: 'parent'},[
+React.createElement('div',{id:'child'}, 
+    [
+        React.createElement('h1', {}, 'text for h1'),
+        React.createElement('h2', {}, 'text for child 12')
+    ]),
 
-    React.createElement('div',{id:'child2'},
-        [
-            React.createElement('h1', {}, 'text for h1'),
-            React.createElement('h2', {}, 'text for child 2')
-        ])
-    ]
-   )
+React.createElement('div',{id:'child2'},
+    [
+        React.createElement('h1', {}, 'text for h1'),
+        React.createElement('h2', {}, 'text for child 2')
+    ])
+]
+)
+
+const jsxheading = <h1 id='heading'>React using JSX</h1>
+
+const HeadingComponent = () =>  (<h1>Heading Component</h1>)
+
+
+const Title = () => {
+    return (
+        <div id='container'>
+            <HeadingComponent />
+            <p>This is a paragraph inside the container div.</p>
+        </div>
+        
+        
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(<HeadingComponent>);
     
-root.render(parent);
+root.render(<Title/>);
---
+++ README.md
@@ -0,0 +1,21 @@
+# Learning session with Namaste react
+
+# parcel
+- created a dev build
+- created a local server
+- refreshing your server
+    - 🔝HMR = Hot Module Replacement
+- file watching algorithm - written in C++
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundle
+- Compress
+- Consistent Hashing (a distributed hashing scheme that operates independently of the number of servers or objects in a distributed hash table by assigning them a position on an abstract circle, or hash ring. This allows servers and objects to scale without affecting the overall system.)
+- Code splitting
+- Differential Bundling - support older browsers
+- Diagnostic (it finds out error in the program)
+- Error handling ( it displays the error in the program)
+- HTTPs (it gives a way to test app on https)
+- Tree Shaking (remove unused code)
+- Different dev & prod buldles
